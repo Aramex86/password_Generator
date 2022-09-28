58865d73a1b9f7781aea8894386f5d03ff2c9a82
Veaceslav <slavbez@mail.ru>
2022-09-28 15:04:36 +0300

    change logic of strength

--- src/components/GeneratorBox/Strength.tsx
+++ src/components/GeneratorBox/Strength.tsx
@@ -3,36 +3,26 @@ import { AppContext } from "../../store/context";
 import style from "./generator.module.css";
 
 export const Strength = () => {
-  const passwordLength = useContext(AppContext);
+  const { options } = useContext(AppContext);
+
+  const strength: boolean[] = [];
+
+  for (let x in options) {
+    strength.push(options[x as keyof typeof options]);
+  }
 
   return (
     <div className={style.strength}>
       <div>Strength</div>
       <div>
-        <div
-          className={`${
-            passwordLength.characterLenght >= 2
-              ? style.scaleActive
-              : style.scale
-          } `}
-        ></div>
-        <div
-          className={`${
-            passwordLength.characterLenght > 5 ? style.scaleActive : style.scale
-          } `}
-        ></div>
-        <div
-          className={`${
-            passwordLength.characterLenght > 8 ? style.scaleActive : style.scale
-          } `}
-        ></div>
-        <div
-          className={`${
-            passwordLength.characterLenght >= 10
-              ? style.scaleActive
-              : style.scale
-          } `}
-        ></div>
+        {strength
+          .sort((a: any, b: any) => b - a)
+          .map((el, i) => (
+            <div
+              className={`${el ? style.scaleActive : style.scale} `}
+              key={i}
+            ></div>
+          ))}
       </div>
     </div>
   );
